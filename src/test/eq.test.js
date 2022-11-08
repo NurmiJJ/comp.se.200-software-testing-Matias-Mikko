e3e4f0a0fb6eae8ad47faf9bcace4c7a8b189892
mikko_moisio <mikko.moisio@tuni.fi>
2022-11-08 14:57:12 +0200

    testing lock file deleted

--- src/test/eq.test.js
+++ src/test/eq.test.js
@@ -7,6 +7,6 @@ const expect = chai.expect
 describe("string object is equal", () => {
     it("Testing string object to match", () =>{
         //expect(function() {eq("6","6")})
-        expect(eq("3","3")).to.equal(true)
+        expect(eq(2,2)).to.equal(true)
     });
    })
