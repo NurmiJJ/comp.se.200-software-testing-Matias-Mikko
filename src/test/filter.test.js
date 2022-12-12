7b6f644cc903b0fbff5e3135d366f10aef33c8de
miggo <miggo@pop-os.localdomain>
2022-12-12 14:55:06 +0200

    done all tests for filter.js

--- src/test/filter.test.js
+++ src/test/filter.test.js
@@ -4,18 +4,30 @@ import filter from "../filter.js"
 const expect = chai.expect
 const should = chai.should()
 
-const users = [{ 'user': 'barney', 'active': true },
-    { 'user': 'fred',   'active': false }];
 
-const items = [{ 'item': 'es', 'sale': true }, 
-{ 'item': 'playstation5', 'sale': false }]
+const items = [{ 'item': 'es', 'sale': false, 'edible': true }, 
+{ 'item': 'playstation5', 'sale': false, 'electronic': true },
+{ 'item': 'flextape', 'sale': true, 'shippable': false },
+{ 'item': 'xboxx', 'electronic': true, 'shippable': true },
+{ 'item': 'xboxx', 'electronic': true, 'shippable': true}]
 
 
 describe("Filter tests", () => {
-    it("Doesn't have the property", () =>{
-        
-        expect(filter(items, ({ sale }) => !sale)).to.eql([{'item': 'playstation5', 'sale': false }])
-   
-        //expect(filter(users, ({ active }) => active)).to.equal(new Array({ 'user': 'barney', 'active': true }));
+    
+    it("Item doesn't have the searched property", () =>{
+        expect(filter(items[0], ({ sale }) => sale)).to.eql([[]])
+    });
+
+    it("Item has the searched property", () =>{
+        expect(filter(items, ({ sale }) => sale)).to.eql([{ 'item': 'flextape', 'sale': true, 'shippable': false}])
+    });
+
+    it("Item category doen't exist on any item", () =>{
+        expect(filter(items, ({ food }) => food)).to.eql([[]])
+    });
+
+    it("Items array have a duplicate item", () =>{
+        expect(filter(items, ({ shippable }) => shippable)).to.eql([{ 'item': 'xboxx', 'electronic': true, 'shippable': true },
+        { 'item': 'xboxx', 'electronic': true, 'shippable': true}])
     });
    })
