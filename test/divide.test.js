33526f342b2d58e28fcf59ca04514962aadce0dc
miggo <miggo@pop-os.localdomain>
2022-12-14 11:27:22 +0200

    done tests for divide.js

---
+++ test/divide.test.js
@@ -0,0 +1,44 @@
+import chai, { AssertionError } from "chai"
+import divide from "../src/divide.js"
+
+const expect = chai.expect
+const should = chai.should()
+const assert = chai.assert
+
+describe("Divide tests", () => {
+
+    it("Dividing with zero", () => {
+        expect(divide(10, 0)).to.eql(NaN)
+    });
+
+    it("Dividing negative number with negative number", () => {
+        expect(divide(-5, -5)).to.equal(1)
+    });
+
+    /*
+    -- Failing
+    it("Dividing negative number with positive number", () =>{
+        expect(divide(-5,5)).to.equal(-1)
+    });
+    */
+
+    it("Dividing numbers that have periodic endeless number as a result", () => {
+        expect(divide(4, 3)).to.equal(1)
+    });
+
+    /*
+    -- Failing
+    it("Divisor is a very big number"", () =>{
+        expect(divide(0,99999999999)).to.equal(0)
+    });
+    */
+
+    /*
+    -- Failing
+    it("Dividend is a very big number", () =>{
+        expect(divide(99999999999,1)).to.equal(99999999999)
+    });
+    */
+
+
+})
