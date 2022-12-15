e990d3809367ce501ebae3de84d710568d7da7ac
miggo <miggo@pop-os.localdomain>
2022-12-15 20:28:54 +0200

    fixed memoize test

--- test/memoize.test.js
+++ test/memoize.test.js
@@ -3,21 +3,16 @@ import memoize from "../src/memoize.js"
 
 const expect = chai.expect
 
-
+const object = { 'a': 1, 'b': 2 }
+const other = { 'c': 3, 'd': 4 }
 
 describe("Memoize tests", () => {
-    /*
-    -- Not working yet
+
     it("Testing the memoize", () =>{
-
-        const object = { 'a': 1, 'b': 2 }
-        const other = { 'c': 3, 'd': 4 }
-
-        const values = memoize(values)
+        const values = memoize(memoize)
         values.cache.set(object, ['a', 'b'])
 
-        expect(values(object)).to.equal(['a', 'b'])
+        expect(values(object)).to.eql(['a', 'b'])
     });
-    */
 
 });
