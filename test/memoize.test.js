1e245fd8ae415d80c9fc3f79ed9367448498bb09
Matias Nurmi <matias.nurmi@tuni.fi>
2022-12-15 19:44:05 +0200

    Add not working memoize test

---
+++ test/memoize.test.js
@@ -0,0 +1,23 @@
+import chai from "chai"
+import memoize from "../src/memoize.js"
+
+const expect = chai.expect
+
+
+
+describe("Memoize tests", () => {
+    /*
+    -- Not working yet
+    it("Testing the memoize", () =>{
+
+        const object = { 'a': 1, 'b': 2 }
+        const other = { 'c': 3, 'd': 4 }
+
+        const values = memoize(values)
+        values.cache.set(object, ['a', 'b'])
+
+        expect(values(object)).to.equal(['a', 'b'])
+    });
+    */
+
+});
