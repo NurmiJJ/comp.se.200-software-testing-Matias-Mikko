d6a07d1521418502866b6c8ae356f3d02adf4cbd
miggo <miggo@pop-os.localdomain>
2022-12-15 12:20:21 +0200

    added more get.js tests to achieve 100% coverage

--- test/get.test.js
+++ test/get.test.js
@@ -9,6 +9,18 @@ const assert = chai.assert
 const items = { 'category': [{ 'SaladFarmer2': { 'price': 2.99 } }] }
 const items2 = {}
 
+// const object = { 'a': [{ 'b': { 'c': 3 } }] }
+//
+// get(object, 'a[0].b.c')
+// // => 3
+//
+// get(object, ['a', '0', 'b', 'c'])
+// // => 3
+//
+// get(object, 'a.b.c', 'default')
+// // => 'default'
+///
+
 describe("Get tests", () => {
 
     it("Default value is not set and search value is undefined", () => {
@@ -26,4 +38,14 @@ describe("Get tests", () => {
     it("When trying to get from empty object", () => {
         should.exist(expect(get(items2, 'category[0].SaladFarmer2.price')).to.equal())
     });
+
+    // EXTRAS which weren't planned
+    it("Testing getting with array of steps", () => {
+        should.exist(expect(get(items, ['category', '0', 'SaladFarmer2','price'])).to.eql(2.99))
+    });
+
+    it("Testing getting from null object", () => {
+        should.exist(expect(get(null, ['category', '0', 'SaladFarmer2','price'])).to.eql())
+    });
+
 })
