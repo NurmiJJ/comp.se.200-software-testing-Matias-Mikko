c7cfbf79ea7a8e80ffe1d3af647169b71fc7fe7e
Matias Nurmi <matias.nurmi@tuni.fi>
2022-12-16 14:46:04 +0200

    Add more test for Memoize

--- test/memoize.test.js
+++ test/memoize.test.js
@@ -4,7 +4,7 @@ import memoize from "../src/memoize.js"
 const expect = chai.expect
 
 const object = { 'a': 1, 'b': 2 }
-const other = { 'c': 3, 'd': 4 }
+const other = ["abc", "123"]
 
 describe("Memoize tests", () => {
 
@@ -15,4 +15,31 @@ describe("Memoize tests", () => {
         expect(values(object)).to.eql(['a', 'b'])
     });
 
+    it("Modify cache with unoriginal size", () =>{
+        const values = memoize(memoize)
+        values.cache.set(object, ['a'])
+
+        expect(values(object)).to.deep.equal(['a'])
+    });
+
+    it("Modify cache to null", () =>{
+        const values = memoize(memoize)
+        values.cache.set(other, null)
+
+        expect(values(other)).to.equal(null)
+    });
+
+    it("Modify cache with same values", () =>{
+        const values = memoize(memoize)
+        values.cache.set(other, ['abc', "123"])
+
+        expect(values(other)).to.deep.equal(['abc', "123"])
+    });
+
+
+    // EXTRAS which weren't planned
+    it("Value is not a function", () =>{
+        expect(function() {memoize("abc")}).to.throw(TypeError)
+    });
+    
 });
